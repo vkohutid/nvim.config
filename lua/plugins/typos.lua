eea5ef6b0b49afd57f47329603fd4d7e211d2be0
Vitalii Kohut <vkohut@intelliarts.com>
2024-06-10 09:25:40 +0300

    fix: remove redundant typos config

--- lua/plugins/typos.lua
+++ lua/plugins/typos.lua
@@ -1,12 +1,3 @@
 return {
   "tekumara/typos-lsp",
-  config = function() end,
-  init_options = {
-    -- Custom config. Used together with any workspace config files, taking precedence for
-    -- settings declared in both. Equivalent to the typos `--config` cli argument.
-    config = "~/code/typos-lsp/crates/typos-lsp/tests/typos.toml",
-    -- How typos are rendered in the editor, eg: as errors, warnings, information, or hints.
-    -- Defaults to error.
-    diagnosticSeverity = "Error",
-  },
 }
